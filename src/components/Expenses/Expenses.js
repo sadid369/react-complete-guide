2329e895618c52e88eaa87aef24bf54770711419
Sadid <sadid.jones@gmail.com>
2023-03-23 22:50:25 +0600

    @

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import Card from "../UI/Card";
+
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className={`expenses`}>
+      {
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+      }
       {props.items.map((item) => {
-        return (
-          <ExpenseItem
-            key={item.id}
-            // title={item.title}
-            // amount={item.amount}
-            // date={item.date}
-            item={item}
-          ></ExpenseItem>
-        );
+        return <ExpenseItem key={item.id} item={item}></ExpenseItem>;
       })}
     </Card>
   );
